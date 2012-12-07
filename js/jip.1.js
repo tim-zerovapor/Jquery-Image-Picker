a060a0e5c0fee564afad8361926eb07b07d04abc
Tim Smith <tsmith@bfgcom.com>
2012-12-07 09:12:44 -0500

    solid version

--- js/jip.1.js
+++ js/jip.1.js
@@ -1,9 +1,17 @@
-/*!
- * jQuery lightweight plugin boilerplate
- * Original author: @ajpiano
- * Further changes, comments: @addyosmani
- * Licensed under the MIT license
+/**
+ *  Jquery Image Picker
+ *  Version 2.0 
+ *  Author : Tim Smith  tim@thinklikearobot.com
+ *   Licensed under the MIT License
+ *   Jquery  Boilerplate Authored by @ajpiano
  */
+
+
+/**
+ * This is a complete rewrite of the orginal  Jquery image picker .
+ * Everythign has been refactored and is now more stable. 
+ */
+
 
 // the semi-colon before the function invocation is a safety
 // net against concatenated scripts and/or other plugins
@@ -33,7 +41,7 @@
             save_btn_text : "Select Image",
             btn_text : "Select Image",
             paging : true,
-            per_page : 10, 
+            per_page : 10 
         };
 
     // The actual plugin constructor
@@ -58,31 +66,43 @@
     Plugin.prototype = {
         
         init: function() {
-            // Place initialization logic here
-            // You already have access to the DOM element and
-            // the options via the instance, e.g. this.element
-            // and this.options
-            // you can add more functions like the one below and 
-            // call them like so: this.yourOtherFunction(this.element, this.options).
-            addLayout(this.element, this.options);     
+                // Place initialization logic here
+                // You already have access to the DOM element and
+                // the options via the instance, e.g. this.element
+                // and this.options
+                // you can add more functions like the one below and 
+                // call them like so: this.yourOtherFunction(this.element, this.options).
+                addLayout(this.element, this.options);     
         }, 
-     
+        
+        /**
+         * moves the modal onto the screen
+         * @param  {object} $elm Modal we want to show
+         */
         showModal :function($elm){
 
-            var target  = $elm.data('target');
+                var target  = $elm.data('target');
 
-            this.getData(function(){
-               $('.jip_modal').modal('show').data('target',target);
-            });
-            
-             return false;
+                this.getData(function(){
+                   $('.jip_modal').modal('show').data('target',target);
+                });
+                
+                 return false;
         },
 
+        /**
+         * Closes our modal when it is open
+         */
         closeModal : function(){
-            $('.jip_modal').modal('hide');
-            return false;
+                $('.jip_modal').modal('hide');
+                return false;
         },
 
+        /**
+         * Uses the predefined url to get data from a server side call. Expects a non-nested JSON object 
+         * @param  {Function} callback [description]
+         * @return {null}            Does not return anything.
+         */
         getData : function(callback){
                 var opts = this.options;
                 var paging = this.options.paging;
@@ -92,8 +112,11 @@
                 
                 $elm.html("<ul></ul>")
 
-                $.getJSON(opts.data, function(items){      
+                $.getJSON(opts.data, function(items){       
+
+                        if(items && typeof(items) == "object"){
                           _this.insertImages(items, $elm,  _this.createPaging);
+                        }
                 });
 
                 $elm.append('<div class="clearfix">&nbsp;</div>');  // add clearfix to make sure their are no float issues.
@@ -103,16 +126,23 @@
                 }  
         },
 
+        /**
+         *  parses our json object and then inserts thoes images into our dom.
+         * @param  {obj}   items    JSON object cotaing our images {'image.jpg','image2.jpg'}
+         * @param  {obj}   $elm     This it the element we are going to insert 
+         * @param  {function} callback  Just incase we need a callback function after images are inserted.
+         * @return { null }            return nothing. 
+         */
         insertImages : function(items , $elm , callback){
                 var opts = this.options;
                 var itemCount = $(items).size();
                 var count = 0 
-                $elm.html("");
+                $elm.find("ul").html("");
 
                 
                  $.each(items, function(i,item){
                     var imgstring = "<li class=\"pickerImage\"><img src=\""+ opts.galleryUrl + item + "\" alt=\"" + item + "\"/></li>"+"\n";
-                    $elm.append(imgstring);
+                    $elm.find("ul").append(imgstring);
                     count = count + 1 ;
                     if(count == itemCount){
                         if (callback && typeof(callback) === "function") {  
@@ -120,47 +150,97 @@
                         }  
                     }
                 });
+                 $(".jip_modal .modal-body ul:eq(0)").css('display','block');
 
         },
+
+        /**
+         * If set to true creating paging for our modal. 
+         * @param  {object} opts default options defined by the plugin
+         * @return {null}      nothing is returned
+         */
         createPaging : function(opts){
 
-          var newcontent = "<ul>";
+                if(!opts.paging){
+                  return;
+                }
 
+                var newcontent = "<ul class=\"page\">";
+                var perPage =  opts.per_page;
+                var count = 1 ; 
 
-          var perPage =  opts.per_page;
-          var count = 1 ; 
+                 $(".modal-body li").each(function(){
+                             
+                             newcontent +=   "<li class=\"pickerImage\">" + $(this).html() + "</li>"; 
 
-      console.log($(".modal-body li").size()  + "  size? "); 
+                             if(count == opts.per_page){
+                                   newcontent += "</ul> <ul class=\"page\">";
+                                   count = 0; 
+                             }
 
+                             count = count + 1; 
+                  });
 
-           $(".modal-body li").each(function(){
-                        newcontent += $(this).html(); 
+                newcontent +="</ul>" ;
 
-                        if(count == opts.per_page){
-                             newcontent += "</ul> <ul>";
-                             count = 0; 
-                        }
+                newcontent += "<div class=\"clearfix\">&nbsp;</div>";
 
-                        count = count + 1; 
-            });
+                $(".jip_modal .modal-body").html(newcontent);
+     
 
-          newcontent +="</ul>" ;
+               var links = '<ul class="pager"><li class="previous disabled"><a href="#">&larr; Previous</a> </li> <li class="next">  <a href="#">Next &rarr;</a> </li></ul>';
 
-          newcontent += "<div class=\"clearfix\">&nbsp;</div>";
+                $(".jip_modal .modal-body").append(links);
 
-          $(".jip_modal .modal-body").html(newcontent);
+                $(".jip_modal .modal-body ul:eq(0)").css('display','block');
 
-         // create links 
-         //          
+        },
 
-         var links = '<ul class="pager"><li class="previous"><a href="#">&larr; Previous</a> </li> <li class="next">  <a href="#">Next &rarr;</a> </li></ul>';
+        /**
+         * Listener for page change. Calculates current page and then transitions to the next page. 
+         * @param  {string} direction You can change the page "up "(+1) or "down "(-1) 
+         * @return {null}           Returns nothing
+         */
+        changePage : function(direction){
+                var elm =  $(".modal-body");
+                var min = 0 ;
+                var max = elm.find('.page').size() - 1;
+                if( typeof currentPage == 'undefined'){
+                  currentPage = 0 ; 
+                  }
 
-          $(".jip_modal .modal-body").append(links);
+                  
+
+                  if(direction == "up"){
+                    currentPage = currentPage + 1 ;
+                  }
+                  else if(direction == "down"){
+                    currentPage = currentPage - 1; 
+                  } 
+
+                  elm.find('.disabled').removeClass("disabled");
+
+                  if( currentPage <= min ){
+                      currentPage = min; 
+                      elm.find(" .previous").addClass("disabled");
+                  }
+                  if(currentPage >= max ){
+                    currentPage =max; 
+                     elm.find(" .next ").addClass("disabled");
+                  }
+
+                  elm.find('.page').hide();
+                  elm.find(".page:eq(" + currentPage + ")").fadeIn();
 
         }
 
     };
 
+      /**
+       * adds jip layout to each input its request for.
+       * @param {object} el      This is the targeted element
+       * @param {object} options Default options for this element
+       */
       var addLayout = function(el,options){
             var target = $(el).attr('name');
             var inputval = $(el).val();
@@ -180,7 +260,6 @@
 
                 $(layout).data("target", target);
 
-
                 var $targetiInput = $('input[name="'+target+'"]');
 
                    $targetiInput.hide();
@@ -189,6 +268,10 @@
             addModal(options);
       }
 
+      /**
+       * Adds the modal to the bottom of the page
+       * @param {obj} options 
+       */
       var addModal = function(options){
 
               modal   = "";
@@ -196,16 +279,9 @@
               modal += "    <div class=\"modal-header\">";
               modal += "      <button type=\"button\" class=\"close\" data-dismiss=\"modal\" aria-hidden=\"true\">&times;<\/button>";
               modal += "      <h3>"+options.modal_title+"<\/h3>";
-              // modal += "      <ul class=\"nav nav-tabs\">";
-              // modal += "          <li class=\"active\"><a href=\"#jip_gallery\"  data-toggle=\"tab\">Gallery</a></li>";
-              // modal += "          <li><a href=\"#jip_upload\" data-toggle=\"tab\">Upload</a></li>";
-              // modal += "      </ul>";
               modal += "    <\/div>";
               modal += "    <div class=\"modal-body\">";
-              // modal += "        <div class=\"tab-content\">";
               modal += "               <div class=\"tab-pane active\" id=\"jip_gallery\"><ul><\/ul></div>";
-              // modal += "               <div class=\"tab-pane\" id=\"jip_upload\">coming soon.. </div>";
-              // modal += "         </div>";
               modal += "    <\/div>";
               modal += "    <div class=\"modal-footer\">";
               modal += "      <a href=\"#\" class=\"btn close\" data-dismiss=\"modal\">Close<\/a>";
@@ -219,7 +295,12 @@
               }
       }
 
-      var selectImg = function( $elm , target){
+ /**
+  * [selectImg description]
+  * @param  {[type]} $elm [description]
+  * @return {[type]}      [description]
+  */
+      var selectImg = function( $elm){
              $('.jip_modal .pickerImage .active').removeClass('active');
              $elm.addClass("active");
       }
@@ -249,7 +330,7 @@
 
                 // onclick for select image button
                 $('.jip_wrapper[rel="'+target+'"] .select_btn').on('click',function(e){
-                    e.preventDefault();
+                 
                     $jip.showModal($(this));
                    
                 });
@@ -259,13 +340,24 @@
                 
                 // selecting a image in modal 
                 $('.jip_modal .pickerImage img').live('click',function(e){
-                      e.preventDefault();
+                  e.preventDefault(); 
                       selectImg($(this), target);
                 });
 
                 $(".jip_save").on('click', function(e){
                     e.preventDefault(); 
                     setImg();
+                });
+
+                $(".jip_modal .modal-body .pager .previous:not(.disabled)").die('click').live('click' , function(e){
+                      e.preventDefault(); 
+                      $jip.changePage("down");
+                    });
+
+
+                $(".jip_modal .modal-body .pager .next:not(.disabled)").die('click').live('click',function(e){
+                  e.preventDefault(); 
+                    $jip.changePage("up");
                 });
 
             }
